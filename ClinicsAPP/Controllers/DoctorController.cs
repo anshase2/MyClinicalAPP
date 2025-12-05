3ad30457fc8543fad9f0b0268f3da0213c31524a
Osama <osamaanshase321@gmail.com>
2025-12-05 13:32:37 +0300

    initial commit

---
+++ ClinicsAPP/Controllers/DoctorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ClinicsAPP.Models;
+
+namespace ClinicsAPP.Controllers
+{
+    public class DoctorController : Controller
+    {
+        [HttpGet("/Doctorlist")]
+        public IActionResult Index()
+        {
+            var doctors = new List<Doctor> { new Doctor { FullName = "osama anshasi" ,Specalist = "Heart" },
+            new Doctor { FullName = "osama anshasi" ,Specalist = "Heart",Description="aa"},
+            new Doctor { FullName = "osama anshasi" ,Specalist = "Heart" },
+            new Doctor { FullName = "osama anshasi" ,Specalist = "Heart" } };
+            return View( doctors);
+        }
+    }
+}
---
+++ ClinicsAPP/Models/Doctor.cs
@@ -0,0 +1,10 @@
+﻿namespace ClinicsAPP.Models
+{
+    public class Doctor
+    {
+        public string FullName { set; get; }
+        public string Specalist { set; get; }
+        public string Description { set; get; }
+
+    }
+}
